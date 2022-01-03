bdbaef6d8b227687ff783951ec7a6efe62d5d5b0
狐日泽 <Sun_Set1102@outlook.com>
2022-01-04 06:56:55 +0800

    Update So_JB_Cool_Tools.py

--- So_JB_Cool_Tools.py
+++ So_JB_Cool_Tools.py
@@ -106,23 +106,15 @@ Del_file = ".minecraft\\config"
 try:
     if not os.path.exists(Backup_Path):
         os.makedirs(Backup_Path)
-        os.system('copy %s %s' % (Backup_File,Con_Backup))
-        shutil.rmtree(Del_file)
-        os.makedirs(Del_file)
-        os.system('Xcopy %s %s /E/H/C' % (Con_Back_Path,Del_file))
-        echo = 'Fix is complete, please run hmcl to start the game.'
-        print(echo)
-        time.sleep(5)
-        sys.exit(echo)
-    else:
-        os.system('copy %s %s' % (Backup_File,Con_Backup))
-        shutil.rmtree(Del_file)
-        os.makedirs(Del_file)
-        os.system('Xcopy %s %s /E/H/C' % (Con_Back_Path,Del_file))
-        echo = 'Fix is complete, please run hmcl to start the game.'
-        print(echo)
-        time.sleep(5)
-        sys.exit(echo)
+        
+    os.system('copy %s %s' % (Backup_File,Con_Backup))
+    shutil.rmtree(Del_file)
+    os.makedirs(Del_file)
+    os.system('Xcopy %s %s /E/H/C' % (Con_Back_Path,Del_file))
+    echo = 'Fix is complete, please run hmcl to start the game.'
+    print(echo)
+    time.sleep(5)
+    sys.exit(echo)
 except:
     logging.debug(traceback.format_exc())
 
